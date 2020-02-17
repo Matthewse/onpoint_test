289dc84b4b1a456234a31b5f05d80e34b2e491b8
Matthew <m-semenets@mail.ru>
2020-02-17 19:05:28 +0300

    Final

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -4,31 +4,27 @@ const app = () => {
    const secondBlock = document.querySelector('.second-block');
 
    const values = [0];
-   let rangeValue;
 
    rangeInput.addEventListener('input', () => {
-      rangeValue = range.value;
+      let rangeValue = range.value;
       values.push(rangeValue);
-
-      firstBlock.style.transition = "0.7s ease";
-      secondBlock.style.transition = "0.7s ease";
 
       if (values[values.length - 2] < rangeValue) {
          if (rangeValue > 25 && rangeValue < 75) {
-            firstBlock.style.marginLeft = '-1024px';
+            firstBlock.classList.add('first-block__invisible');
          } else if (rangeValue > 75) {
-            secondBlock.style.marginLeft = '-1024px';
-            firstBlock.style.marginLeft = '-1024px';
+            secondBlock.classList.add('second-block__invisible');
+            firstBlock.classList.add('first-block__invisible');
          }
       } else {
          if (rangeValue > 25 && rangeValue < 75) {
-            secondBlock.style.marginLeft = '0';
+            secondBlock.classList.remove('second-block__invisible');
          } else if (rangeValue < 75) {
-            firstBlock.style.marginLeft = '0';
-            secondBlock.style.marginLeft = '0';
+            firstBlock.classList.remove('first-block__invisible');
+            secondBlock.classList.remove('second-block__invisible');
          }
       }
-   })
+   });
 }
 
 export default app;
--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,3 @@
-import app from './modules/app';
+import app from './app/app';
 
 document.addEventListener('DOMContentLoaded', app());
